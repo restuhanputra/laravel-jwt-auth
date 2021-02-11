6f67173126fdbae783508a78d71c6e582f286058
restuhanputra <restu.hanputraa@gmail.com>
2021-02-11 16:57:59 +0700

    add route API's

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+// use Tymon\JWTAuth\Contracts\JWTSubject;
+
 use Tymon\JWTAuth\Contracts\JWTSubject;
-class User extends Authenticatable
+
+class User extends Authenticatable implements JWTSubject
+// class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
